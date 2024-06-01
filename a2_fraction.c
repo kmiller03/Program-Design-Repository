4a01d7ddae1f64283646c4bb7615821e468870a4
kmiller03 <161364401+kmiller03@users.noreply.github.com>
2024-06-01 13:07:44 -0400

    Update a2_fraction.c

--- a2_fraction.c
+++ a2_fraction.c
@@ -5,6 +5,7 @@
 
 int main() {
     int numerator, denominator, simplifiedNumerator, simplifiedDenominator, numeratorModDenominator, denominatorModNumerator;
+    
     do {
         //Takes inputs & breaks if 0. Takes mod.
         printf("Enter a fraction (enter 0 to stop):");
@@ -12,41 +13,38 @@ int main() {
         if (numerator == 0 || denominator == 0) {
             break;
         }
-
-        //Tests and outputs if the fraction simplifies to a whole number
         numeratorModDenominator = numerator % denominator;
         denominatorModNumerator = denominator % numerator;
-        //printf("1: %d, 2: %d\n", numeratorModDenominator, denominatorModNumerator);
+
+        //Tests and outputs if the fraction simplifies to a whole number
         if (numeratorModDenominator == 0) {
             simplifiedNumerator = numerator / denominator;
             printf("In lowest terms: %d\n", simplifiedNumerator);
             continue;
         }
-        else if (numeratorModDenominator == 0) {
+        //Case for fractions with numerator that simplifies to 1
+        else if(denominatorModNumerator == 0) {
             simplifiedNumerator = 1;
-            simplifiedDenominator = denominator / numerator;
-        }
-        else if(numeratorModDenominator != 0 && denominatorModNumerator == 0) {
-            simplifiedNumerator = numerator / numeratorModDenominator;
             simplifiedDenominator = denominator / numeratorModDenominator;
         }
-        else if(denominatorModNumerator != 0) {
+        //Case for fractions with a greatest common denominator
+        else if(denominatorModNumerator != 0 && (denominator % denominatorModNumerator) == 0) {
             simplifiedNumerator = numerator / denominatorModNumerator;
             simplifiedDenominator = denominator / denominatorModNumerator;
         }
+        //Case for already simplified fractions
         else {
             simplifiedNumerator = numerator;
             simplifiedDenominator = denominator;
         }
-
+        //Simplifies negative denominators
+        if (simplifiedDenominator < 0) {
+            simplifiedNumerator *= -1;
+            simplifiedDenominator *= -1;
+        }
+        
+        //Output
         printf("In lowest terms: %d/%d\n", simplifiedNumerator, simplifiedDenominator);
-
-        //Three cases: whole number, unsimplifiable, and denominator is a multiple of the numerator
-        //Need to move - denominators to - numerators
-        //Need to account for numerators and denominators with GCD's. Can probably do GCD = numerator % denominator or vice versa as a final step given that its no other case.
-
-
-
     } while (numerator != 0 && denominator != 0);
 
 
