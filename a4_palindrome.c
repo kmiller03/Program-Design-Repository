56be2ee04c7f0b5ccbe0ecdeb56c6dee70585572
kmiller03 <161364401+kmiller03@users.noreply.github.com>
2024-06-26 19:45:55 -0400

    Create a4_palindrome.c

---
+++ a4_palindrome.c
@@ -0,0 +1,79 @@
+//Name: Kadin Miller 
+//NetID: kadinmiller
+//Description: Takes a string input and prints whether or not it is a palindrome.
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#define INPUT_LENGTH 100
+
+int main() {
+char terminateProgram = 'a';
+int isPalindrome = 1;
+int count;
+
+char ch;
+char inputMessage[INPUT_LENGTH + 1];
+char *inputPointer;
+char *inversePointer;
+
+while (terminateProgram != 'N' && terminateProgram != 'n') {
+    
+    //Takes input and clears any newlines, spaces, or special characters while doing so
+    printf("Enter a message: ");
+    count = 0;
+    inputPointer = inputMessage;
+    while ( ( (ch = getchar() ) != '\n') && count < INPUT_LENGTH) {
+        if ( (ch != ' ') && (   /*lowercase */ (ch >= 'a' && ch <= 'z') || /* or uppercase */ (ch >= 'A' && ch <= 'Z')   ) ) {
+            *inputPointer = ch;
+            inputPointer++;
+            count++;
+        }
+        if (count > INPUT_LENGTH) {
+            printf("Error: input exceeded");
+            return 0;
+        }
+    }
+    *inputPointer = '\0';
+
+    //Test if the inputted message is a palindrome
+    isPalindrome = 1;
+    inversePointer = inputMessage;
+    inputPointer--;
+    for (int i = 0; i < count / 2; i++) {
+        if (*inversePointer != *inputPointer) {
+            isPalindrome = 0;
+            break;
+        }
+        inversePointer++;
+        inputPointer--;
+    }
+
+    //Outputs whether or not a palindrome was detected
+    if (isPalindrome == 0) {
+        printf("Not a palindrome\n\n");
+    }
+    else {
+        printf("Palindrome\n\n");
+    }
+
+    //Clears input array
+    for (inputPointer = inputMessage; inputPointer < inputMessage + INPUT_LENGTH; inputPointer++) {
+        *inputPointer = '\0';
+    }
+
+    //Terminates the program if an N is inputted as first char
+    printf("Do you want to continue (N to stop)? ");
+    while ( (terminateProgram = getchar()) ) { //how do I ignore any whitespace or newlines without using isalpha? 
+        if (isalpha(terminateProgram) || isdigit(terminateProgram) ) {
+            break;
+        }
+    }
+    if (terminateProgram == 'n') {break;}
+    while ((terminateProgram = getchar()) != '\n') {
+        if (terminateProgram == 'n') { //hacky.
+            break;
+        }
+    }
+}
+    return 0;
+}
