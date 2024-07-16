a50e51f2f881911e5a683e7f269549d6bc1c62a5
kmiller03 <161364401+kmiller03@users.noreply.github.com>
2024-07-16 00:35:50 +0000

    update

---
+++ assignment6.c
@@ -0,0 +1,23 @@
+// Name: Kadin Miller
+// NetID: kadinmiller
+// Description: Creates and tracks up to 10 bank accounts
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXIMUM_NAME_LENGTH 30
+
+struct bank_account{
+    int accountNumber;
+    char accountType;
+    char accountOwner[MAXIMUM_NAME_LENGTH];
+    char openDate[10];
+    int balance;
+};
+
+
+int main() {
+
+
+
+    return 0;
+}
---
+++ dump.c
@@ -0,0 +1,2 @@
+printf("%s %s %-30s %-18s %s\n", "####", "Type", "Holder name", "Open date", "Balance");
+printf("%04d  %c   %-30s %04d-%02d-%02d %15.2f\n", //insert your vars here );
